0d5441805297ffe6021bd1b7b068ff75cc0b3ca7
giosali <70548443+giosali@users.noreply.github.com>
2024-01-14 00:44:19 -0600

    Set application version

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QCoreApplication>
 #include <QLocale>
 #include <QTranslator>
 #include <QtGlobal>
 
 int main(int argc, char *argv[])
 {
-#ifdef Q_OS_LINUX
     // FMI: https://doc.qt.io/qt-5/qtglobal.html
-    //
+#ifdef Q_OS_LINUX
     // Ensures application runs on Xwayland on Linux.
     qputenv("QT_QPA_PLATFORM", "xcb");
 #endif
 
     QApplication a(argc, argv);
+    QCoreApplication::setApplicationVersion(PROJECT_VERSION);
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
