d8b7670674fd37d46b4fa4e818b91d41b60bcca0
giosali <70548443+giosali@users.noreply.github.com>
2024-01-13 19:11:08 -0600

    Ensure window is centered on instantiation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QScreen>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    move(screen()->geometry().center() - frameGeometry().center());
 }
 
 MainWindow::~MainWindow()
