ade414089f58b44393e0f0712c523b25266da2dc
giosali <70548443+giosali@users.noreply.github.com>
2024-01-16 13:30:09 -0600

    Create FindMainWindow function

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,23 +1,33 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+
+#include <QApplication>
+#include <QObject>
 #include <QScreen>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+#include "./ui_mainwindow.h"
 
-    // Centers the window.
-    move(screen()->geometry().center() - frameGeometry().center());
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
 
-    // Removes space between Central Widget and the window.
-    // FMI: https://stackoverflow.com/a/24240025/18831815
-    centralWidget()->layout()->setContentsMargins(0, 0, 0, 0);
-    statusBar()->hide();
+  // Centers the window.
+  move(screen()->geometry().center() - frameGeometry().center());
+
+  // Removes space between Central Widget and the window.
+  // -> FMI: https://stackoverflow.com/a/24240025/18831815
+  centralWidget()->layout()->setContentsMargins(0, 0, 0, 0);
+  statusBar()->hide();
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() { delete ui; }
+
+QMainWindow* MainWindow::FindMainWindow() {
+  for (const auto& widget : QApplication::topLevelWidgets()) {
+    QMainWindow* main_window = qobject_cast<QMainWindow*>(widget);
+    if (main_window != nullptr) {
+      return main_window;
+    }
+  }
+
+  return nullptr;
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -11,13 +11,16 @@ QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+ public:
+  MainWindow(QWidget *parent = nullptr);
 
-private:
-    Ui::MainWindow *ui;
+  ~MainWindow();
+
+  static QMainWindow *FindMainWindow();
+
+ private:
+  Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
