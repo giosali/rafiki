de43b422569c22bf39c2f1ce6e0593c61abb8e2d
giosali <70548443+giosali@users.noreply.github.com>
2024-02-22 20:12:45 -0600

    Edit comments

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -12,7 +12,7 @@ MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
 
-  // Centers the window.
+  // Centers the window. On Linux, this will only work on X11 or XWayland.
   move(screen()->geometry().center() - frameGeometry().center());
 
   // Removes space between Central Widget and the window.
@@ -25,8 +25,8 @@ MainWindow::MainWindow(QWidget* parent)
   // Hides the QListViewsince it will be empty on initialization.
   findChild<QListView*>("listView")->hide();
 
-  // Resizes the MainWindow to the initial width of the MainWindow and the
-  // height of the MainWindow's layout.
+  // Sets the height of MainWindow to the height of MainWindow's layout.
+  // Leaves the width unchanged by setting it to its own width.
   QRect layoutGeometry = this->centralWidget()->layout()->contentsRect();
   resize(geometry().width(), layoutGeometry.height());
 }
