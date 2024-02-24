5dac6be1246a6e11fa232b9210edb16594e83dd5
giosali <70548443+giosali@users.noreply.github.com>
2024-02-24 12:41:51 -0600

    Convert SearchBox to a QWidget

--- src/searchbox.cpp
+++ src/searchbox.cpp
@@ -3,10 +3,21 @@
 #include <QObject>
 #include <QtGlobal>
 
-SearchBox::SearchBox(QWidget* parent) : QLineEdit(parent) {
-  QObject::connect(this, &QLineEdit::textChanged, this,
+#include "./ui_searchbox.h"
+
+SearchBox::SearchBox(QWidget* parent)
+    : QWidget(parent), ui(std::make_unique<Ui::SearchBox>()) {
+  ui->setupUi(this);
+
+  // Removes spacing around the search box.
+  ui->layout->setSpacing(0);
+  ui->layout->setContentsMargins(0, 0, 0, 0);
+
+  QObject::connect(ui->searchBox, &QLineEdit::textChanged, this,
                    &SearchBox::TextChanged);
 }
+
+SearchBox::~SearchBox() {}
 
 void SearchBox::TextChanged(const QString& text) {
   qDebug() << "Text changed: " << text;
--- src/searchbox.h
+++ src/searchbox.h
@@ -1,16 +1,28 @@
 #ifndef SEARCHBOX_H
 #define SEARCHBOX_H
 
-#include <QLineEdit>
+#include <QWidget>
+#include <memory>
 
-class SearchBox : public QLineEdit {
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class SearchBox;
+}
+QT_END_NAMESPACE
+
+class SearchBox : public QWidget {
   Q_OBJECT
 
  public:
-  SearchBox(QWidget* parent = nullptr);
+  explicit SearchBox(QWidget* parent = nullptr);
+
+  ~SearchBox();
 
  public slots:
   void TextChanged(const QString& text);
+
+ private:
+  std::unique_ptr<Ui::SearchBox> ui;
 };
 
 #endif  // SEARCHBOX_H
