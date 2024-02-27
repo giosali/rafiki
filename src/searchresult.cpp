e743921b012d4a9c7c7ad43a597e73db714526a2
giosali <70548443+giosali@users.noreply.github.com>
2024-02-26 19:02:30 -0600

    Add comment

--- src/searchresult.cpp
+++ src/searchresult.cpp
@@ -26,6 +26,9 @@ void SearchResult::SetIcon(const QString& path) const {
   // If the height of the icon is greater than the fixed height of the search
   // result, the icon height will take precedence over the fixed height, thus
   // overriding the fixed height.
+  //
+  // For more information regarding blurry icons:
+  // https://github.com/qbittorrent/qBittorrent/issues/8841#issuecomment-387179854
   auto icon = QIcon(path);
   ui_->icon->setPixmap(icon.pixmap(kFixedHeight));
 }
