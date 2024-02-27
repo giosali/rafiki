02eb2d6813798536c00629a31768613da46a3e9e
giosali <70548443+giosali@users.noreply.github.com>
2024-02-27 17:10:21 -0600

    Move translations to top-level directory

---
+++ translations/rafiki_en_US.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>hello</source>
+        <translation type="vanished">There</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchBox</name>
+    <message>
+        <location filename="searchbox.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchResult</name>
+    <message>
+        <location filename="searchresult.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="searchresult.ui" line="20"/>
+        <location filename="searchresult.ui" line="29"/>
+        <location filename="searchresult.ui" line="36"/>
+        <location filename="searchresult.ui" line="58"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
