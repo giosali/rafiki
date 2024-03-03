f3b00ddb19e2ff9011c99a963a2bf1af4254194d
giosali <70548443+giosali@users.noreply.github.com>
2024-03-02 21:11:50 -0600

    Add ProjectIO object

---
+++ src/projectio.cpp
@@ -0,0 +1,56 @@
+#include "projectio.h"
+
+#include <QDebug>
+#include <QFile>
+#include <QIODevice>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <iterator>
+#include <utility>
+
+#include "websearch.h"
+
+ProjectIO::ProjectIO() {
+  ParseDataFile<WebSearch>(kDataDir + kWebSearchesFile);
+}
+
+std::vector<std::unique_ptr<DataModel>> ProjectIO::FindDataModels(
+    const QString& input) {
+  auto suggestions = autocomplete_map_.Find(input);
+  auto data_models = std::vector<std::unique_ptr<DataModel>>();
+  for (const auto& suggestion : suggestions) {
+    auto data_model_it = data_model_map_.find(suggestion);
+    if (data_model_it == data_model_map_.end()) {
+      continue;
+    }
+
+    auto dms = std::move(data_model_it->second);
+    data_models.insert(data_models.end(), std::make_move_iterator(dms.begin()),
+                       std::make_move_iterator(dms.end()));
+  }
+
+  return data_models;
+  // auto value = data_model_map_.find(key);
+  // return value == data_model_map_.end()
+  //            ? std::vector<std::unique_ptr<DataModel>>()
+  //            : std::move(value->second);
+}
+
+template <typename T>
+void ProjectIO::ParseDataFile(const QString& path) {
+  auto file = QFile(path);
+  file.open(QIODevice::ReadOnly | QIODevice::Text);
+  auto doc = QJsonDocument::fromJson(file.readAll());
+
+  auto arr = doc.array();
+  for (auto it = arr.begin(); it != arr.end(); ++it) {
+    auto data_model = std::make_unique<T>();
+    data_model->Populate(it->toObject());
+
+    auto key = data_model->GetKey();
+    autocomplete_map_.Insert(key);
+    data_model_map_[key].push_back(std::move(data_model));
+  }
+}
---
+++ src/projectio.h
@@ -0,0 +1,30 @@
+#ifndef PROJECTIO_H
+#define PROJECTIO_H
+
+#include <QString>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+#include "autocompletemap.h"
+#include "datamodel.h"
+
+class ProjectIO {
+ public:
+  const QString kDataDir = "://data/";
+  const QString kWebSearchesFile = "web-searches.json";
+
+  explicit ProjectIO();
+
+  std::vector<std::unique_ptr<DataModel>> FindDataModels(const QString& input);
+
+ private:
+  template <typename T>
+  void ParseDataFile(const QString& path);
+
+  AutocompleteMap autocomplete_map_;
+  std::unordered_map<QString, std::vector<std::unique_ptr<DataModel>>>
+      data_model_map_;
+};
+
+#endif  // PROJECTIO_H
