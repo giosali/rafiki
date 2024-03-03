09d8faebee9a29b7039fbed0a6f04a40f0df9b1b
giosali <70548443+giosali@users.noreply.github.com>
2024-03-02 21:11:27 -0600

    Add WebSearch object

---
+++ src/websearch.cpp
@@ -0,0 +1,40 @@
+#include "websearch.h"
+
+#include <QJsonValue>
+
+void WebSearch::Populate(const QJsonObject& object) {
+  auto id_val = object["id"];
+  auto id = id_val.toInt();
+  id_ = id;
+
+  auto key_val = object["key"];
+  auto key = key_val.toString();
+  SetKey(key);
+
+  auto icon_val = object["icon"];
+  auto icon = icon_val.toString();
+  SetIcon(icon);
+
+  auto url_val = object["url"];
+  auto url = url_val.toString();
+  url_ = url;
+
+  auto title_val = object["title"];
+  auto title = title_val.toString();
+  SetTitle(title);
+
+  auto description_val = object["description"];
+  auto description = description_val.toString();
+  SetDescription(description);
+
+  auto alt_val = object["alt"];
+  auto alt = alt_val.toObject();
+
+  auto alt_url_val = alt["url"];
+  auto alt_url = alt_url_val.toString();
+  alt_url_ = alt_url;
+
+  auto alt_title_val = alt["title"];
+  auto alt_title = alt_title_val.toString();
+  alt_title_ = alt_title;
+}
---
+++ src/websearch.h
@@ -0,0 +1,26 @@
+#ifndef WEBSEARCH_H
+#define WEBSEARCH_H
+
+#include <QJsonObject>
+#include <QString>
+
+#include "datamodel.h"
+
+class WebSearch : public DataModel {
+ public:
+  explicit WebSearch() = default;
+
+  ~WebSearch() = default;
+
+  void Populate(const QJsonObject& object) override;
+
+ private:
+  QString alt_url_;
+  QString alt_title_;
+  int id_;
+  QString key_;
+  QString placeholder_;
+  QString url_;
+};
+
+#endif  // WEBSEARCH_H
