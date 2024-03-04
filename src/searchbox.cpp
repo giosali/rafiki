925cb4470807c81b2eea7bac7e40e0350aa2fad0
giosali <70548443+giosali@users.noreply.github.com>
2024-03-03 23:08:10 -0600

    Remove comments

--- src/searchbox.cpp
+++ src/searchbox.cpp
@@ -11,7 +11,6 @@ SearchBox::SearchBox(QWidget* parent)
   // Removes spacing around the search box.
   ui_->layout->setSpacing(0);
   ui_->layout->setContentsMargins(0, 0, 0, 0);
-  // qDebug() << "FROM CONSTRUCTOR" << Height();
   setFixedHeight(Height());
 
   QObject::connect(ui_->searchBox, &QLineEdit::textChanged, this,
@@ -20,11 +19,7 @@ SearchBox::SearchBox(QWidget* parent)
 
 SearchBox::~SearchBox() {}
 
-int SearchBox::Height() const {
-  return ui_->layout->sizeHint().height();
-  // return ui_->layout->geometry().height();
-  // return ui_->searchBox->height();
-}
+int SearchBox::Height() const { return ui_->layout->sizeHint().height(); }
 
 void SearchBox::SetText(const QString& text) {
   if (text == text_) {
