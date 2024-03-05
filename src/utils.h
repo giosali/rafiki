c76d34d4dcaa799344eab3346367e7b4295864d3
giosali <70548443+giosali@users.noreply.github.com>
2024-03-05 12:26:58 -0600

    Create utils file

---
+++ src/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <QString>
+
+namespace utils {
+inline QString Format(QString fmt, const QString& arg) {
+  return fmt.replace(fmt.indexOf("{}"), 2, arg);
+}
+
+}  // namespace utils
+
+#endif  // UTILS_H
