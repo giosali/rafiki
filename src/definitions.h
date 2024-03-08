787ae0fa92db563a5e75851e0df6d68bf96c6c9f
giosali <70548443+giosali@users.noreply.github.com>
2024-03-08 15:35:39 -0600

    Add Action::DeactivateMainWindow

--- src/definitions.h
+++ src/definitions.h
@@ -2,7 +2,7 @@
 #define DEFINITIONS_H
 
 namespace defs {
-enum class Action { Nothing, SetTextToCommand };
+enum class Action { DeactivateMainWindow, Nothing, SetTextToCommand };
 }
 
 #endif  // DEFINITIONS_H
