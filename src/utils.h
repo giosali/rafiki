24fcb4d0cb1b31ddc901ce0aaabb6a412604fcf1
giosali <70548443+giosali@users.noreply.github.com>
2024-03-14 00:04:58 -0500

    Add Capitalize function

--- src/utils.h
+++ src/utils.h
@@ -5,6 +5,10 @@
 
 namespace utils {
 
+inline QString Capitalize(QString str) {
+  return str.replace(0, 1, str.at(0).toUpper());
+}
+
 inline QString Format(QString fmt, const QString& arg) {
   return fmt.replace(fmt.indexOf("{}"), 2, arg);
 }
