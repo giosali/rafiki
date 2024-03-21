17841706b57ef63649128879f714685eb6febe8d
giosali <70548443+giosali@users.noreply.github.com>
2024-03-21 08:31:39 -0500

    Replace QObject with QWidget

--- src/interactable.h
+++ src/interactable.h
@@ -2,16 +2,16 @@
 #define INTERACTABLE_H
 
 #include <QKeyCombination>
-#include <QObject>
 #include <QString>
+#include <QWidget>
 
 class Interactable {
  public:
   virtual QString DragAndDrop() = 0;
   virtual void ProcessKeyPress(const QKeyCombination& combination,
-                               QObject* parent) = 0;
+                               QWidget* parent) = 0;
   virtual void ProcessKeyRelease(const QKeyCombination& combination,
-                                 QObject* parent) = 0;
+                                 QWidget* parent) = 0;
 };
 
 #endif  // INTERACTABLE_H
