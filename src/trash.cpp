10dd51fccb37a050a793c72bf165cbd50b976c51
giosali <70548443+giosali@users.noreply.github.com>
2024-03-27 09:35:24 -0500

    Add Trash class

---
+++ src/trash.cpp
@@ -0,0 +1,60 @@
+#include "trash.h"
+
+#include <Qt>
+#include <cstdlib>
+#include <filesystem>
+#include <vector>
+
+#include "searchresultlist.h"
+
+Trash::Trash()
+    : BaseResult{kId,          kIcon,    kTitle,      kAltTitle,
+                 kDescription, kCommand, kPlaceholder} {
+  if (auto xdg_data_home = std::getenv("XDG_DATA_HOME");
+      xdg_data_home != nullptr) {
+    auto home_loc = std::string{xdg_data_home};
+    trash_loc_ =
+      home_loc.empty() ? "~/.local/share/Trash" : home_loc + "/Trash";
+  }
+}
+
+QString Trash::DragAndDrop() { return QString{}; }
+
+void Trash::ProcessKeyPress(const QKeyCombination& combination,
+                            QWidget* parent) {
+  auto search_result_list = dynamic_cast<SearchResultList*>(parent);
+  if (search_result_list == nullptr) {
+    return;
+  }
+
+  switch (combination.key()) {
+    case Qt::Key_Tab:
+      if (auto command = FormatCommand();
+          search_result_list->GetCmd() != command) {
+        emit search_result_list->TextReceived(command);
+      }
+
+      break;
+
+    case Qt::Key_Return:
+      search_result_list->HideParent();
+      Empty();
+      break;
+  }
+}
+
+void Trash::ProcessKeyRelease(const QKeyCombination& combination,
+                              QWidget* parent) {
+  return;
+}
+
+void Trash::Empty() const {
+  auto trash_path = std::filesystem::path{trash_loc_};
+  auto paths = std::vector<std::filesystem::path>{trash_path / "files",
+                                                  trash_path / "info"};
+  for (const auto& path : paths) {
+    for (const auto& entry : std::filesystem::directory_iterator(path)) {
+      std::filesystem::remove_all(entry.path());
+    }
+  }
+}
---
+++ src/trash.h
@@ -0,0 +1,37 @@
+#ifndef TRASH_H
+#define TRASH_H
+
+#include <QKeyCombination>
+#include <QString>
+#include <QWidget>
+#include <string>
+
+#include "baseresult.h"
+#include "project.h"
+
+class Trash : public BaseResult {
+ public:
+  Trash();
+
+  QString DragAndDrop() override;
+  void ProcessKeyPress(const QKeyCombination& combination,
+                       QWidget* parent) override;
+  void ProcessKeyRelease(const QKeyCombination& combination,
+                         QWidget* parent) override;
+
+ private:
+  static const inline QString kAltTitle{};
+  static const inline QString kCommand{"empty trash"};
+  static const inline QString kDescription{};
+  static const inline QString kIcon{
+    Project::GetImageFilePath(defs::ImageFile::kTrash)};
+  static const inline QString kId{"9056b806-9219-4f6a-b180-bb087b892bae"};
+  static const inline QString kPlaceholder{};
+  static const inline QString kTitle{"Empty trash"};
+
+  void Empty() const;
+
+  std::string trash_loc_;
+};
+
+#endif  // TRASH_H
