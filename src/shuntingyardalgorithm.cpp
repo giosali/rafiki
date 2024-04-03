877c3b050bf17987ad9571a3446e75fdf1884a9f
giosali <70548443+giosali@users.noreply.github.com>
2024-04-03 18:12:01 -0500

    Add files

---
+++ src/shuntingyardalgorithm.cpp
@@ -0,0 +1,277 @@
+#include "shuntingyardalgorithm.h"
+
+#include <QLocale>
+#include <cmath>
+#include <limits>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+
+std::optional<std::string> ShuntingYardAlgorithm::TryParse(
+  const std::string& input) {
+  auto locale = QLocale{};
+  auto postfix_expression =
+    ParseInfixExpression(input, locale.decimalPoint().toStdString());
+  if (!postfix_expression.has_value()) {
+    return {};
+  }
+
+  return ParsePostfixExpression(postfix_expression.value());
+}
+
+bool ShuntingYardAlgorithm::IsNumber(char ch) {
+  switch (ch) {
+    case '.':
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      return true;
+    default:
+      return false;
+  }
+}
+
+std::optional<std::queue<std::string>>
+ShuntingYardAlgorithm::ParseInfixExpression(
+  const std::string& expression, const std::string& decimal_separator) {
+  auto output = std::queue<std::string>{};
+  auto operators = std::stack<char>{};
+  auto buffer = std::string{};
+
+  for (size_t i = 0; i < expression.length(); ++i) {
+    auto token = expression[i];
+    switch (token) {
+      // Ignores all spaces.
+      case ' ':
+        break;
+      case '(':
+        if (!buffer.empty()) {
+          output.push(buffer);
+          buffer.clear();
+        }
+
+        // Handles multiplication through parentheses. If there's a
+        // number in the output and no operators in the stack, then that means
+        // something like the following was encountered: 2(2 + 2)
+        if (!output.empty() && operators.empty() &&
+            IsNumber(output.back()[0])) {
+          operators.push('*');
+        }
+
+        operators.push(token);
+        break;
+      case ')':
+        if (!buffer.empty()) {
+          output.push(buffer);
+          buffer.clear();
+        }
+
+        // Pops out all operators from the stack and pushes them to the output
+        // until a left bracket is encountered.
+        for (auto top = operators.top(); !operators.empty() && top != '(';
+             operators.pop(), top = operators.top()) {
+          output.push(std::string{top});
+        }
+
+        // Removes the left parenthesis '('.
+        operators.pop();
+        break;
+      case '!':
+      case '*':
+      case '+':
+      case '-':
+      case '/':
+      case '^':
+        // Checks if the buffer contains text. The buffer is a number so if it
+        // contains text, then it must be pushed to the output before handling
+        // the current operator.
+        if (!buffer.empty()) {
+          output.push(buffer);
+          buffer.clear();
+        }
+
+        // While the top of the stack of operators has greater precedence than
+        // the current operator, pop them and push them to the queue.
+        for (; !operators.empty(); operators.pop()) {
+          auto top = operators.top();
+          if (Precedence(top) <= Precedence(token)) {
+            break;
+          }
+
+          output.push(std::string{top});
+        }
+
+        // If the current token is a minus sign and the operator at the
+        // top of the stack of operators is also a minus sign, then
+        // replace the top operator with a plus sign.
+        // Otherwise, If the current token is a minus sign and the operator at
+        // the top of the stack of operators is a plus sign, then replace the
+        // top operator with a minus sign.
+        if (!operators.empty() && token == '-') {
+          auto top = operators.top();
+          switch (top) {
+            case '-':
+            case '+':
+              operators.pop();
+              operators.push(',' * 2 - top);
+              continue;
+          }
+        }
+
+        operators.push(token);
+        break;
+      case '.':
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        buffer += token;
+        break;
+      default:
+        return {};
+    }
+  }
+
+  // Make sure to always push numbers to the queue before pushing remaining
+  // operators to the queue.
+  if (!buffer.empty()) {
+    output.push(buffer);
+  }
+
+  // Pushes remaining operators to queue.
+  for (; !operators.empty(); operators.pop()) {
+    auto top = operators.top();
+    if (top == ')') {
+      continue;
+    }
+
+    output.push(std::string{operators.top()});
+  }
+
+  return output;
+}
+
+std::string ShuntingYardAlgorithm::ParsePostfixExpression(
+  std::queue<std::string>& expression) {
+  auto numbers = std::stack<double>{};
+  for (; !expression.empty(); expression.pop()) {
+    auto token = expression.front();
+
+    // If the length of the string is greater than 1, then it is a number and
+    // not an operator since operators will only consume one character.
+    if (token.length() > 1) {
+      try {
+        numbers.push(std::stod(token));
+      } catch (const std::out_of_range&) {
+        numbers.push(std::numeric_limits<double>::infinity());
+      }
+
+      continue;
+    }
+
+    // At this point, it could be either a one-digit integer or an operator
+    // since the token has a length of 1.
+    auto ch = token[0];
+    switch (ch) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        numbers.push(ch - '0');
+        continue;
+    }
+
+    // At this point, the token can only be an operator.
+
+    // If there are no numbers at this point (which means there's no right-side
+    // operand), then something went wrong.
+    if (numbers.empty()) {
+      return "";
+    }
+
+    auto right_operand = numbers.top();
+    switch (ch) {
+      case '!':
+        numbers.pop();
+        numbers.push(std::tgamma(right_operand + 1));
+        continue;
+    }
+
+    // We need to have at least two numbers in order to continue.
+    if (numbers.size() == 1) {
+      return "";
+    }
+
+    // At this point, there are at least two numbers in the stack which means we
+    // can perform left and right side operations.
+    // auto right_operand = numbers.top();
+    numbers.pop();
+    auto left_operand = numbers.top();
+    numbers.pop();
+    switch (ch) {
+      case '*':
+        numbers.push(left_operand * right_operand);
+        break;
+      case '+':
+        numbers.push(left_operand + right_operand);
+        break;
+      case '-':
+        numbers.push(left_operand - right_operand);
+        break;
+      case '/':
+        if (right_operand == 0) {
+          numbers.push(std::numeric_limits<double>::quiet_NaN());
+          break;
+        }
+
+        numbers.push(left_operand / right_operand);
+        break;
+      case '^':
+        numbers.push(std::pow(left_operand, right_operand));
+        break;
+    }
+  }
+
+  // Converting a double to a string using stringstream is favorable to
+  // converting through std::to_string because the latter appends up to 6 zeros
+  // at the end.
+  auto stream = std::stringstream{};
+  stream << numbers.top();
+  return stream.str();
+}
+
+int ShuntingYardAlgorithm::Precedence(char op) {
+  switch (op) {
+    case '!':
+      return 4;
+    case '^':
+      return 3;
+    case '*':
+    case '/':
+      return 2;
+    case '+':
+    case '-':
+      return 1;
+    default:
+      return 0;
+  }
+}
---
+++ src/shuntingyardalgorithm.h
@@ -0,0 +1,21 @@
+#ifndef SHUNTINGYARDALGORITHM_H
+#define SHUNTINGYARDALGORITHM_H
+
+#include <optional>
+#include <queue>
+#include <string>
+
+class ShuntingYardAlgorithm {
+ public:
+  static std::optional<std::string> TryParse(const std::string& input);
+
+ private:
+  static bool IsNumber(char ch);
+  static std::optional<std::queue<std::string>> ParseInfixExpression(
+    const std::string& expression, const std::string& decimal_separator);
+  static std::string ParsePostfixExpression(
+    std::queue<std::string>& expression);
+  static int Precedence(char op);
+};
+
+#endif  // SHUNTINGYARDALGORITHM_H
