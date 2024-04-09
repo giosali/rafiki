96637f73792e7f2ea1380db95c57fa15df692d5d
giosali <70548443+giosali@users.noreply.github.com>
2024-04-09 14:11:06 -0500

    Rename test to tests

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ cmake -E make_directory build
 cd build
 
 # Specifies that the CMakeLists.txt file to be used is the
-# one in ../test
-cmake -DCMAKE_PREFIX_PATH="~/Qt/6.6.1/gcc_64/lib/cmake" -DCMAKE_BUILD_TYPE=Release ../test
+# one in ../tests
+cmake -DCMAKE_PREFIX_PATH="~/Qt/6.6.1/gcc_64/lib/cmake" -DCMAKE_BUILD_TYPE=Release ../tests
 
 cmake --build .
 cd ..
