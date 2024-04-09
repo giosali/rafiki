6da0a836c5d7221eb7a956c05114e816846a4470
giosali <70548443+giosali@users.noreply.github.com>
2024-04-09 18:50:49 -0500

    Create ProcessedResult class

---
+++ src/models/processedresult.cpp
@@ -0,0 +1,1 @@
+#include "processedresult.h"
---
+++ src/models/processedresult.h
@@ -0,0 +1,14 @@
+#ifndef PROCESSEDRESULT_H
+#define PROCESSEDRESULT_H
+
+#include "../core/input.h"
+#include "baseresult.h"
+
+class ProcessedResult : public BaseResult {
+ public:
+  using BaseResult::BaseResult;
+
+  virtual bool ProcessInput(const Input& input) = 0;
+};
+
+#endif  // PROCESSEDRESULT_H
