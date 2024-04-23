36848dab349b728e6c68a15a33adddbb3f71521d
giosali <70548443+giosali@users.noreply.github.com>
2024-04-23 09:51:39 -0500

    Create Strip function

--- src/core/utils.h
+++ src/core/utils.h
@@ -2,6 +2,7 @@
 #define UTILS_H
 
 #include <QString>
+#include <string>
 
 namespace utils {
 
@@ -14,6 +15,20 @@ inline QString Format(QString fmt, const QString& arg) {
 }
 
 inline char ToLower(char c) { return c < 'A' || c > 'Z' ? c : c + 32; }
+
+inline std::string Strip(const std::string& input, char token = '\0') {
+  auto start_it = input.begin();
+  for (char ch = *start_it; std::isspace(ch) || ch == token;
+       ++start_it, ch = *start_it) {
+  }
+
+  auto end_it = input.rbegin();
+  for (char ch = *end_it; std::isspace(ch) || ch == token;
+       ++end_it, ch = *end_it) {
+  }
+
+  return std::string{start_it, end_it.base()};
+}
 
 }  // namespace utils
 
