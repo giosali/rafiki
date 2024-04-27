2659e1c71bff1fe8f499e4e1bd7010019492fdf7
giosali <70548443+giosali@users.noreply.github.com>
2024-04-26 22:52:48 -0500

    Add tests for Split function

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -1,5 +1,6 @@
 #include <cctype>
 #include <string>
+#include <vector>
 
 #include "../src/core/utils.h"
 #include "utest.h"
@@ -55,6 +56,33 @@ UTEST(ToLower, returns_lowercase_characters) {
   auto ascii_uppercase = std::string{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
   for (auto ch : ascii_uppercase) {
     EXPECT_TRUE_MSG(std::tolower(ch) == utils::ToLower(ch), &ch);
+  }
+}
+
+UTEST(Split, string_with_multiple_tokens) {
+  auto str = std::string{"hello:there:world"};
+  const auto expected = std::vector<std::string>{"hello", "there", "world"};
+  auto actual = utils::Split(str, ':');
+  for (size_t i = 0; i < expected.size(); ++i) {
+    ASSERT_TRUE(expected[i] == actual[i]);
+  }
+}
+
+UTEST(Split, string_with_no_tokens) {
+  auto str = std::string{"hello world"};
+  const auto expected = std::vector<std::string>{"hello world"};
+  auto actual = utils::Split(str, ':');
+  for (size_t i = 0; i < expected.size(); ++i) {
+    ASSERT_TRUE(expected[i] == actual[i]);
+  }
+}
+
+UTEST(Split, string_with_one_token) {
+  auto str = std::string{"hello:world"};
+  const auto expected = std::vector<std::string>{"hello", "world"};
+  auto actual = utils::Split(str, ':');
+  for (size_t i = 0; i < expected.size(); ++i) {
+    ASSERT_TRUE(expected[i] == actual[i]);
   }
 }
 
