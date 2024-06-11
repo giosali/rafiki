7bdd0d7d2b05003e551b176350ca56bf674dedc7
giosali <70548443+giosali@users.noreply.github.com>
2024-06-10 21:32:42 -0500

    Fix instantiation of Autocompleter

--- tests/test_autocompletemap.cpp
+++ tests/test_autocompletemap.cpp
@@ -10,9 +10,9 @@
 UTEST_MAIN();
 
 UTEST(Find, return_matching_sets) {
-  auto autocomplete_map = AutocompleteMap();
+  auto autocompleter = Autocompleter();
   auto term = QString("hello");
-  autocomplete_map.Insert(term);
+  autocompleter.Insert(term);
 
   auto expected = std::set<QString>();
   expected.insert(term);
@@ -20,28 +20,28 @@ UTEST(Find, return_matching_sets) {
   auto key = QString();
   for (const auto ch : term) {
     key += ch;
-    auto actual = autocomplete_map.Find(Input{key});
+    auto actual = autocompleter.Find(Input{key});
     ASSERT_TRUE(expected == actual);
   }
 }
 
 UTEST(Find, return_empty_set) {
-  auto autocomplete_map = AutocompleteMap();
+  auto autocompleter = Autocompleter();
   auto term = QString("hello");
-  autocomplete_map.Insert(term);
+  autocompleter.Insert(term);
 
-  auto actual = autocomplete_map.Find(Input{"bye"});
+  auto actual = autocompleter.Find(Input{"bye"});
   ASSERT_TRUE(actual.empty());
 }
 
 UTEST(Insert, no_duplicates) {
-  auto autocomplete_map = AutocompleteMap();
+  auto autocompleter = Autocompleter();
   auto term = QString("hello");
   for (size_t i = 1; i < 3; i++) {
-    autocomplete_map.Insert(term);
+    autocompleter.Insert(term);
   }
 
   auto expected = 1;
-  auto actual = autocomplete_map.Find(Input{term}).size();
+  auto actual = autocompleter.Find(Input{term}).size();
   ASSERT_EQ(expected, actual);
 }
