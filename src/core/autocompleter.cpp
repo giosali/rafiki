a36697cb4b87a6f14921e11c2440812d4c910c8d
giosali <70548443+giosali@users.noreply.github.com>
2024-06-11 19:57:01 -0500

    Have Insert function be mindful of acronyms

--- src/core/autocompleter.cpp
+++ src/core/autocompleter.cpp
@@ -20,11 +20,41 @@ std::set<QString> Autocompleter::Find(const Input& input) const {
 
 void Autocompleter::Insert(const QString& text) {
   auto key = QString{};
-  for (const auto ch : text) {
+  auto acronym_key = QString{};
+
+  for (size_t i = 0; i < text.length(); ++i) {
+    auto ch = text[i];
+    if (ch.isUpper()) {
+      ch = ch.toLower();
+
+      // Allows for keys such as "ProtonVPN" to be  accessed, for example,
+      // through of the following consecutive letters: "pvpn"
+      acronym_key += ch;
+    }
+
     key += ch;
 
     // If the set does not exist when calling operator[], it will
     // automatically be default-initialized.
+    // The key for the internal must only consist of lowercase characters; the
+    // value can contain mixed-case.
     map_[key].insert(text);
+  }
+
+  // Allows for the actual "consecutive" part to be implemented (concerning the
+  // acronym key). For example, "pvpn" from "ProtonVPN" breaks down (and is
+  // stored) into the following:
+  // -> "pvpn"
+  // -> "vpn"
+  // -> "pn"
+  // -> "n"
+  for (size_t i = 0, l = acronym_key.length(); i < l; ++i, key.clear()) {
+    auto slice = acronym_key.sliced(i);
+
+    auto key = QString{};
+    for (const auto& ch : slice) {
+      key += ch;
+      map_[key].insert(text);
+    }
   }
 }
