f1eaed140e1ba5062a7c574606db6c1c2e926780
giosali <70548443+giosali@users.noreply.github.com>
2024-06-12 20:12:03 -0500

    Enable parallel compilation

--- tools/test.sh
+++ tools/test.sh
@@ -9,6 +9,7 @@ cd build
 # one in ../tests
 cmake -DCMAKE_PREFIX_PATH="~/Qt/6.6.1/gcc_64/lib/cmake" -DCMAKE_BUILD_TYPE=Release ../tests
 
-cmake --build .
+# -j10 adds parallel compilation.
+cmake --build . -j10
 cd ..
 rm -rf build
