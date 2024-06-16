c26bb1e113b0c92b84b873b2632507d0dfe121b1
giosali <70548443+giosali@users.noreply.github.com>
2024-06-16 14:29:19 -0500

    Create Url model class

---
+++ src/models/url.cpp
@@ -0,0 +1,53 @@
+#include "url.h"
+
+#include <QDesktopServices>
+
+#include "../core/io.h"
+#include "../core/urlparser.h"
+
+Url::Url()
+    : ProcessedResult{
+        kId,       kIcon,        kTitle,   kTitlePlaceholder,
+        kAltTitle, kDescription, kCommand, kAppendSpaceToCommand} {}
+
+bool Url::ProcessInput(const Input& input) {
+  auto parser = UrlParser{input.ToString()};
+  if (!parser.IsValid()) {
+    return false;
+  }
+
+  title_ = parser.Url();
+  return true;
+}
+
+void Url::Drag() {}
+
+void Url::ProcessKeyPress(const QKeyCombination& combination,
+                          const Input& input) {
+  switch (combination.key()) {
+    case Qt::Key_Return: {
+      emit Hidden();
+      QDesktopServices::openUrl(QUrl(title_));
+      break;
+    }
+  }
+}
+
+void Url::ProcessKeyRelease(const QKeyCombination& combination,
+                            const Input& input) {}
+
+const QString Url::kAltTitle{};
+
+const bool Url::kAppendSpaceToCommand{false};
+
+const QString Url::kCommand{};
+
+const QString Url::kDescription{"Open URL"};
+
+const QString Url::kIcon{Io::GetIcon(Io::ImageFile::kUrl)};
+
+const QString Url::kId{"f0638541-f3a0-441d-b821-452429265d07"};
+
+const QString Url::kTitle{};
+
+const QString Url::kTitlePlaceholder{};
---
+++ src/models/url.h
@@ -0,0 +1,33 @@
+#ifndef URL_H
+#define URL_H
+
+#include "processedresult.h"
+
+class Url : public ProcessedResult {
+  Q_OBJECT
+
+ public:
+  Url();
+
+  bool ProcessInput(const Input& input) override;
+
+ public slots:
+  void Drag() override;
+  void ProcessKeyPress(const QKeyCombination& combination,
+                       const Input& input) override;
+  void ProcessKeyRelease(const QKeyCombination& combination,
+                         const Input& input) override;
+
+ private:
+  static const QString kAltTitle;
+  static const bool kAppendSpaceToCommand;
+  static const QString kCommand;
+  static const QString kDescription;
+  static const QString kIcon;
+  static const QString kId;
+  static const QString kInfoDescription;
+  static const QString kTitle;
+  static const QString kTitlePlaceholder;
+};
+
+#endif  // URL_H
