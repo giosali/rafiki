0b7c4ab093184d794db545bac12a005a68e9ef28
giosali <70548443+giosali@users.noreply.github.com>
2024-06-23 22:22:01 -0500

    Populate table with web searches

--- src/ui/settingswindow.cpp
+++ src/ui/settingswindow.cpp
@@ -1,10 +1,77 @@
 #include "settingswindow.h"
 
+#include <QCheckBox>
+#include <QHBoxLayout>
+#include <QHeaderView>
+#include <QIcon>
+#include <QLabel>
+#include <QListWidgetItem>
+#include <QStandardItemModel>
+#include <QString>
+#include <QStringList>
+#include <QTableWidgetItem>
+#include <Qt>
+
+#include "../core/io.h"
 #include "./ui_settingswindow.h"
 
 SettingsWindow::SettingsWindow(QWidget* parent)
     : QMainWindow{parent}, ui_{std::make_unique<Ui::SettingsWindow>()} {
   ui_->setupUi(this);
+
+  ui_->tableWidget->setColumnCount(4);
+  ui_->tableWidget->verticalHeader()->hide();
+  ui_->tableWidget->setHorizontalHeaderLabels(
+    QStringList{} << "Icon" << "Command" << "Example" << "Enabled");
+  ui_->tableWidget->horizontalHeader()->setSectionResizeMode(
+    QHeaderView::ResizeToContents);
+  ui_->tableWidget->horizontalHeader()->setSectionResizeMode(
+    2, QHeaderView::Stretch);
 }
 
 SettingsWindow::~SettingsWindow() {}
+
+void SettingsWindow::closeEvent(QCloseEvent* event) {
+  ui_->tableWidget->clearContents();
+
+  QWidget::closeEvent(event);
+}
+
+void SettingsWindow::showEvent(QShowEvent* event) {
+  const auto web_searches = Io::GetWebSearches();
+  for (size_t i = 0; i < web_searches.size(); ++i) {
+    auto web_search = web_searches[i];
+    ui_->tableWidget->setRowCount(i + 1);
+
+    auto icon_widget = new QWidget{};
+    auto layout = new QHBoxLayout{icon_widget};
+    auto icon_label = new QLabel{};
+    // label->setMaximumSize(16, 16);
+    // label->setScaledContents(true);
+    icon_label->setPixmap(web_search->GetIcon(16));
+    layout->addWidget(icon_label);
+    layout->setAlignment(Qt::AlignCenter);
+    layout->setContentsMargins(0, 0, 0, 0);
+    icon_widget->setLayout(layout);
+    ui_->tableWidget->setCellWidget(i, 0, icon_widget);
+
+    auto command_item = new QTableWidgetItem{web_search->GetCommand()};
+    command_item->setTextAlignment(Qt::AlignCenter);
+    ui_->tableWidget->setItem(i, 1, command_item);
+
+    auto example_item =
+      new QTableWidgetItem{web_search->FormatTitle(QString{})};
+    ui_->tableWidget->setItem(i, 2, example_item);
+
+    auto check_box_widget = new QWidget{};
+    auto check_box_layout = new QHBoxLayout{check_box_widget};
+    auto check_box = new QCheckBox{};
+    check_box_layout->addWidget(check_box);
+    check_box_layout->setAlignment(Qt::AlignCenter);
+    check_box_layout->setContentsMargins(0, 0, 0, 0);
+    check_box_widget->setLayout(check_box_layout);
+    ui_->tableWidget->setCellWidget(i, 3, check_box_widget);
+  }
+
+  QWidget::showEvent(event);
+}
--- src/ui/settingswindow.h
+++ src/ui/settingswindow.h
@@ -1,7 +1,9 @@
 #ifndef SETTINGSWINDOW_H
 #define SETTINGSWINDOW_H
 
+#include <QCloseEvent>
 #include <QMainWindow>
+#include <QShowEvent>
 #include <memory>
 
 QT_BEGIN_NAMESPACE
@@ -17,6 +19,10 @@ class SettingsWindow : public QMainWindow {
   SettingsWindow(QWidget* parent = nullptr);
 
   ~SettingsWindow();
+
+ protected:
+  void closeEvent(QCloseEvent* event) override;
+  void showEvent(QShowEvent* event) override;
 
  private:
   std::unique_ptr<Ui::SettingsWindow> ui_;
