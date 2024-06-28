669aa684f9d857add8450ac77004fe1c5dccae1b
giosali <70548443+giosali@users.noreply.github.com>
2024-06-27 20:55:59 -0500

    Rename UI variables, include YourWebSearches table, and connect checkboxes to ToggleResult

--- src/ui/settingswindow.cpp
+++ src/ui/settingswindow.cpp
@@ -20,19 +20,28 @@ SettingsWindow::SettingsWindow(QWidget* parent)
   ui_->setupUi(this);
 
   auto columns = QStringList{"Icon", "Command", "Example", "Enabled"};
-  ui_->tableWidget->setColumnCount(columns.size());
-  ui_->tableWidget->verticalHeader()->hide();
-  ui_->tableWidget->setHorizontalHeaderLabels(columns);
-  ui_->tableWidget->horizontalHeader()->setSectionResizeMode(
+
+  ui_->webSearchesTableWidget->setColumnCount(columns.size());
+  ui_->webSearchesTableWidget->verticalHeader()->hide();
+  ui_->webSearchesTableWidget->setHorizontalHeaderLabels(columns);
+  ui_->webSearchesTableWidget->horizontalHeader()->setSectionResizeMode(
     QHeaderView::ResizeToContents);
-  ui_->tableWidget->horizontalHeader()->setSectionResizeMode(
+  ui_->webSearchesTableWidget->horizontalHeader()->setSectionResizeMode(
+    2, QHeaderView::Stretch);
+
+  ui_->yourWebSearchesTableWidget->setColumnCount(columns.size());
+  ui_->yourWebSearchesTableWidget->verticalHeader()->hide();
+  ui_->yourWebSearchesTableWidget->setHorizontalHeaderLabels(columns);
+  ui_->yourWebSearchesTableWidget->horizontalHeader()->setSectionResizeMode(
+    QHeaderView::ResizeToContents);
+  ui_->yourWebSearchesTableWidget->horizontalHeader()->setSectionResizeMode(
     2, QHeaderView::Stretch);
 }
 
 SettingsWindow::~SettingsWindow() {}
 
 void SettingsWindow::closeEvent(QCloseEvent* event) {
-  ui_->tableWidget->clearContents();
+  ui_->webSearchesTableWidget->clearContents();
 
   QWidget::closeEvent(event);
 }
@@ -41,7 +50,7 @@ void SettingsWindow::showEvent(QShowEvent* event) {
   const auto web_searches = Io::FilterResults<WebSearch>();
   for (size_t i = 0; i < web_searches.size(); ++i) {
     auto web_search = web_searches[i];
-    ui_->tableWidget->setRowCount(i + 1);
+    ui_->webSearchesTableWidget->setRowCount(i + 1);
 
     // Icon column.
     auto icon_widget = new QWidget{};
@@ -52,22 +61,24 @@ void SettingsWindow::showEvent(QShowEvent* event) {
     layout->setAlignment(Qt::AlignCenter);
     layout->setContentsMargins(0, 0, 0, 0);
     icon_widget->setLayout(layout);
-    ui_->tableWidget->setCellWidget(i, 0, icon_widget);
+    ui_->webSearchesTableWidget->setCellWidget(i, 0, icon_widget);
 
     // Command column
     auto command_item = new QTableWidgetItem{web_search->GetCommand()};
     command_item->setTextAlignment(Qt::AlignCenter);
-    ui_->tableWidget->setItem(i, 1, command_item);
+    ui_->webSearchesTableWidget->setItem(i, 1, command_item);
 
     // Example column.
     auto example_item =
       new QTableWidgetItem{web_search->FormatTitle(QString{})};
-    ui_->tableWidget->setItem(i, 2, example_item);
+    ui_->webSearchesTableWidget->setItem(i, 2, example_item);
 
     // Enabled column.
     auto check_box_widget = new QWidget{};
     auto check_box_layout = new QHBoxLayout{check_box_widget};
     auto check_box = new QCheckBox{};
+    check_box->setCheckState(web_search->IsEnabled() ? Qt::Checked
+                                                     : Qt::Unchecked);
 
     auto id = web_search->GetId();
 
@@ -80,7 +91,7 @@ void SettingsWindow::showEvent(QShowEvent* event) {
     check_box_layout->setAlignment(Qt::AlignCenter);
     check_box_layout->setContentsMargins(0, 0, 0, 0);
     check_box_widget->setLayout(check_box_layout);
-    ui_->tableWidget->setCellWidget(i, 3, check_box_widget);
+    ui_->webSearchesTableWidget->setCellWidget(i, 3, check_box_widget);
   }
 
   QWidget::showEvent(event);
@@ -89,8 +100,10 @@ void SettingsWindow::showEvent(QShowEvent* event) {
 void SettingsWindow::ProcessStateChange(int state, uint64_t id) const {
   switch (state) {
     case Qt::Unchecked:
+      Io::ToggleResult(id, false);
       break;
     case Qt::Checked:
+      Io::ToggleResult(id, true);
       break;
   }
 }
