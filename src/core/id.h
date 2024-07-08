247a7c77646106e32fb0c2c5a786c0c7dda6088d
giosali <70548443+giosali@users.noreply.github.com>
2024-07-07 22:16:06 -0500

    Add Id class

---
+++ src/core/id.h
@@ -0,0 +1,37 @@
+#ifndef ID_H
+#define ID_H
+
+#include <QString>
+#include <QVariant>
+#include <cstdint>
+#include <cstdlib>
+
+class Id {
+ public:
+  Id();
+  explicit Id(uint64_t author_id, uint64_t result_id);
+  explicit Id(const QString& tuple);
+  explicit Id(const QVariant& tuple);
+
+  uint64_t GetAuthorId() const;
+  uint64_t GetResultId() const;
+  QString ToString() const;
+  bool operator==(const Id& id) const;
+
+ private:
+  uint64_t author_id_;
+  uint64_t result_id_;
+};
+
+template <>
+struct std::hash<Id> {
+  size_t operator()(const Id& id) const {
+    auto a = id.GetAuthorId();
+    auto b = id.GetResultId();
+
+    // https://stackoverflow.com/a/13871379
+    return a >= b ? a * a + a + b : a + b * b;
+  }
+};
+
+#endif  // ID_H
