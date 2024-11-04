aeae0b2b5a7ca86fca1c70c7d14618f2466f106b
giosali <70548443+giosali@users.noreply.github.com>
2024-11-03 23:57:54 -0600

    SWap Id type with uint64_t

--- src/ui/searchresultitem.cpp
+++ src/ui/searchresultitem.cpp
@@ -1,6 +1,6 @@
 #include "searchresultitem.h"
 
-SearchResultItem::SearchResultItem(const Id& id, QListWidget* parent)
+SearchResultItem::SearchResultItem(uint64_t id, QListWidget* parent)
     : QListWidgetItem{parent}, id_{id} {}
 
-Id SearchResultItem::GetId() const { return id_; }
+uint64_t SearchResultItem::GetId() const { return id_; }
--- src/ui/searchresultitem.h
+++ src/ui/searchresultitem.h
@@ -5,16 +5,14 @@
 #include <QListWidgetItem>
 #include <cstdint>
 
-#include "../core/id.h"
-
 class SearchResultItem : public QListWidgetItem {
  public:
-  explicit SearchResultItem(const Id& id, QListWidget* parent = nullptr);
+  explicit SearchResultItem(uint64_t id, QListWidget* parent = nullptr);
 
-  Id GetId() const;
+  uint64_t GetId() const;
 
  private:
-  Id id_;
+  uint64_t id_{0};
 };
 
 #endif
