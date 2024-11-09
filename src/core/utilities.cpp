9106bf06b0569508efb0fcf11638537759f3db21
giosali <70548443+giosali@users.noreply.github.com>
2024-11-08 18:18:21 -0600

    Move Split and ToChar utils functions to Utilities class

--- src/core/utilities.cpp
+++ src/core/utilities.cpp
@@ -1,8 +1,48 @@
 #include "utilities.h"
 
+std::vector<QString> Utilities::Split(const QString& str,
+                                      const QChar& delimiter, int maxsplit) {
+  auto tokens = std::vector<QString>{};
+  auto start = size_t{0};
+  auto end = size_t{0};
+  auto splits = 0;
+
+  while (start < str.length()) {
+    // If we've reached maxsplit, add the rest of the string as the last token
+    if (maxsplit >= 0 && splits >= maxsplit) {
+      tokens.push_back(str.mid(start));
+      break;
+    }
+
+    // Find next delimiter
+    end = str.indexOf(delimiter, start);
+
+    // If no more delimiters found, add the rest as the last token
+    if (end == -1) {
+      if (start < str.length()) {
+        tokens.push_back(str.mid(start));
+      }
+      break;
+    }
+
+    // Add token to result
+    if (end - start > 0) {
+      tokens.push_back(str.mid(start, end - start));
+    } else if (end == start) {
+      // Handle empty token between consecutive delimiters
+      tokens.push_back(QString());
+    }
+
+    // Move past the delimiter
+    start = end + 1;
+    ++splits;
+  }
+
+  return tokens;
+}
+
 std::vector<std::string> Utilities::Split(const std::string& str,
-                                          const std::string& delimiter,
-                                          int maxsplit) {
+                                          char delimiter, int maxsplit) {
   auto tokens = std::vector<std::string>{};
   auto start = size_t{0};
   auto end = size_t{0};
@@ -36,7 +76,7 @@ std::vector<std::string> Utilities::Split(const std::string& str,
     }
 
     // Move past the delimiter
-    start = end + delimiter.length();
+    start = end + 1;
     ++splits;
   }
 
--- src/core/utilities.h
+++ src/core/utilities.h
@@ -1,6 +1,7 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
+#include <QString>
 #include <string>
 #include <vector>
 
@@ -8,8 +9,9 @@ class Utilities {
  public:
   Utilities() = delete;
 
-  static std::vector<std::string> Split(const std::string& str,
-                                        const std::string& delimiter,
+  static std::vector<QString> Split(const QString& str, const QChar& delimiter,
+                                    int maxsplit = -1);
+  static std::vector<std::string> Split(const std::string& str, char delimiter,
                                         int maxsplit = -1);
   static char ToLower(char c);
 };
