5868ef51e40d70b52c3ca461a11d2d8037f635ba
giosali <70548443+giosali@users.noreply.github.com>
2024-11-20 14:03:22 -0600

    Check alt key before parsing QJsonObject

--- src/core/models/websearchmodel.cpp
+++ src/core/models/websearchmodel.cpp
@@ -8,17 +8,16 @@
 WebSearchModel::WebSearchModel(const QJsonObject& object)
     : FeatureModel{std::make_unique<WebSearchBridge>()},
       url_{object["url"].toString()} {
-  auto alt = object["alt"].toObject();
-  auto title = object["title"].toString();
-
-  SetAltTitle(alt["title"].toString());
   SetCommand(object["command"].toString());
   SetIcon(object["icon"].toString());
   SetId(object["id"].toString().toULongLong());
-  SetTitle(title);
+  SetTitle(object["title"].toString());
   SetTitlePlaceholder(object["placeholder"].toString());
 
-  alt_url_ = alt["url"].toString();
+  if (object.contains("alt")) {
+    auto alt = object["alt"].toObject();
+    alt_url_ = alt["url"].toString();
+  }
 
   if (auto v = object["isCustom"]; !v.isUndefined()) {
     is_custom_ = v.toBool();
