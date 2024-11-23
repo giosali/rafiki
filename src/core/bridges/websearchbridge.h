956b34da3c486b84615174c4f4b9367cf4b42180
giosali <70548443+giosali@users.noreply.github.com>
2024-11-23 10:48:59 -0600

    Override PassInput function

--- src/core/bridges/websearchbridge.h
+++ src/core/bridges/websearchbridge.h
@@ -4,6 +4,8 @@
 
 class WebSearchBridge final : public FeatureBridge {
  public:
+  std::vector<FeatureObject*> PassInput(const FeatureModel* feature_model,
+                                        const QString& input) const override;
   std::vector<FeatureObject*> ProcessInput(const FeatureModel* feature_model,
                                            const QString& input) override;
 };
