0773e59868fb91082a8618cb4ae7192dcca14696
giosali <70548443+giosali@users.noreply.github.com>
2024-11-23 10:48:46 -0600

    Override PassInput function

--- src/core/bridges/websearchbridge.cpp
+++ src/core/bridges/websearchbridge.cpp
@@ -3,6 +3,12 @@
 #include "../models/websearchmodel.h"
 #include "../objects/websearchobject.h"
 
+std::vector<FeatureObject*> WebSearchBridge::PassInput(
+  const FeatureModel* feature_model, const QString& input) const {
+  auto model = static_cast<const WebSearchModel*>(feature_model);
+  return {new WebSearchObject{model, input, input}};
+}
+
 std::vector<FeatureObject*> WebSearchBridge::ProcessInput(
   const FeatureModel* feature_model, const QString& input) {
   auto model = static_cast<const WebSearchModel*>(feature_model);
