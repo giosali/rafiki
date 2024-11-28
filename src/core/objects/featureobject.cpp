6175f61941aad5f76c2de34d516583a759f57a5e
giosali <70548443+giosali@users.noreply.github.com>
2024-11-28 16:38:58 -0600

    Replace GetClickCount calls with GetUseCount

--- src/core/objects/featureobject.cpp
+++ src/core/objects/featureobject.cpp
@@ -46,13 +46,12 @@ QString FeatureObject::GetTitle() const { return title_; }
 
 bool FeatureObject::operator<(const FeatureObject& rhs) const {
   // First, compare by count in descending order.
-  if (auto left_count = model_->GetClickCount(),
-      right_count = rhs.model_->GetClickCount();
-      left_count != right_count) {
-    return left_count > right_count;
+  if (auto luc = model_->GetUseCount(), ruc = rhs.model_->GetUseCount();
+      luc != ruc) {
+    return luc > ruc;
   }
 
-  // If counts are equal, compare by name in ascending order.
+  // If counts are equal, compare by title in ascending order.
   return title_ < rhs.title_;
 }
 
