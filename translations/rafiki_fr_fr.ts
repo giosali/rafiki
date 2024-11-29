769f09a9bd68546d9a87500fc6935f6b76102614
giosali <70548443+giosali@users.noreply.github.com>
2024-11-29 17:29:11 -0600

    Add translations for French - France

---
+++ translations/rafiki_fr_fr.ts
@@ -0,0 +1,300 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourceLanguage="en">
+  <context>
+    <name>CalculatorObject</name>
+    <message>
+      <source>Copy to clipboard</source>
+      <translation>Copier dans le presse-papiers</translation>
+    </message>
+    <message>
+      <source>Please enter a valid expression</source>
+      <translation>Veuillez entrer une expression valide</translation>
+    </message>
+  </context>
+  <context>
+    <name>ApplicationModel</name>
+    <message>
+      <source>Open containing folder</source>
+      <translation>Ouvrir le dossier parent</translation>
+    </message>
+  </context>
+  <context>
+    <name>FileSystemEntryModel</name>
+    <message>
+      <source>Open containing folder</source>
+      <translation>Ouvrir le dossier parent</translation>
+    </message>
+    <message>
+      <source>Search for files on your PC and open</source>
+      <translation>Rechercher et ouvrir des fichiers sur votre ordinateur</translation>
+    </message>
+    <message>
+      <source>Open file</source>
+      <translation>Ouvrir un fichier</translation>
+    </message>
+  </context>
+  <context>
+    <name>TrashModel</name>
+    <message>
+      <source>Empty trash</source>
+      <translation>Vider la corbeille</translation>
+    </message>
+  </context>
+  <context>
+    <name>UrlModel</name>
+    <message>
+      <source>Open URL</source>
+      <translation>Ouvrir le lien</translation>
+    </message>
+  </context>
+  <context>
+    <name>WebSearchModel</name>
+    <!-- Google -->
+    <message>
+      <source>Search Google for '%1'</source>
+      <translation>Rechercher « %1 » sur Google</translation>
+    </message>
+    <message>
+      <source>https://google.com/search?q=%1</source>
+      <translation>https://google.fr/search?q=%1</translation>
+    </message>
+    <message>
+      <source>https://google.com</source>
+      <translation>https://google.fr</translation>
+    </message>
+    <message>
+      <source>Visit the Google homepage</source>
+      <translation>Consulter la page d'accueil de Google</translation>
+    </message>
+    <!-- DuckDuckGo -->
+    <message>
+      <source>Search DuckDuckGo for '%1'</source>
+      <translation>Rechercher « %1 » sur DuckDuckGo</translation>
+    </message>
+    <message>
+      <source>https://duckduckgo.com/?q=%1</source>
+      <translation>https://duckduckgo.com/?q=%1</translation>
+    </message>
+    <message>
+      <source>https://duckduckgo.com</source>
+      <translation>https://duckduckgo.com</translation>
+    </message>
+    <message>
+      <source>Visit the DuckDuckGo homepage</source>
+      <translation>Consulter la page d'accueil de DuckDuckGo</translation>
+    </message>
+    <!-- Amazon -->
+    <message>
+      <source>Search Amazon for '%1'</source>
+      <translation>Rechercher « %1 » sur Amazon</translation>
+    </message>
+    <message>
+      <source>https://amazon.com/s?k=%1</source>
+      <translation>https://amazon.fr/s?k=%1</translation>
+    </message>
+    <message>
+      <source>https://amazon.com</source>
+      <translation>https://amazon.fr</translation>
+    </message>
+    <message>
+      <source>Visit the Amazon homepage</source>
+      <translation>Consulter la page d'accueil d'Amazon</translation>
+    </message>
+    <!-- eBay -->
+    <message>
+      <source>Search eBay for '%1'</source>
+      <translation>Rechercher « %1 » sur eBay</translation>
+    </message>
+    <message>
+      <source>https://ebay.com/sch/i.html?_nkw=%1</source>
+      <translation>https://ebay.fr/sch/i.html?_nkw=%1</translation>
+    </message>
+    <message>
+      <source>https://ebay.com</source>
+      <translation>https://ebay.fr</translation>
+    </message>
+    <message>
+      <source>Visit the eBay homepage</source>
+      <translation>Consulter la page d'accueil d'eBay</translation>
+    </message>
+    <!-- Facebook -->
+    <message>
+      <source>Search Facebook for '%1'</source>
+      <translation>Rechercher « %1 » sur Facebook</translation>
+    </message>
+    <message>
+      <source>https://facebook.com/search/top?q=%1</source>
+      <translation>https://facebook.com/search/top?q=%1</translation>
+    </message>
+    <message>
+      <source>https://facebook.com</source>
+      <translation>https://facebook.com</translation>
+    </message>
+    <message>
+      <source>Visit the Facebook homepage</source>
+      <translation>Consulter la page d'accueil de Facebook</translation>
+    </message>
+    <!-- GitHub -->
+    <message>
+      <source>Search GitHub for '%1'</source>
+      <translation>Rechercher « %1 » sur GitHub</translation>
+    </message>
+    <message>
+      <source>https://github.com/search?q=%1</source>
+      <translation>https://github.com/search?q=%1</translation>
+    </message>
+    <message>
+      <source>https://github.com</source>
+      <translation>https://github.com</translation>
+    </message>
+    <message>
+      <source>Visit the GitHub homepage</source>
+      <translation>Consulter la page d'accueil de GitHub</translation>
+    </message>
+    <!-- Instagram -->
+    <message>
+      <source>Visit %1's Instagram profile</source>
+      <translation>Consulter le profil Instagram de « %1 »</translation>
+    </message>
+    <message>
+      <source>https://instagram.com/%1</source>
+      <translation>https://instagram.com/%1</translation>
+    </message>
+    <message>
+      <source>https://instagram.com</source>
+      <translation>https://instagram.com</translation>
+    </message>
+    <message>
+      <source>Visit the Instagram homepage</source>
+      <translation>Consulter la page d'accueil d'Instagram</translation>
+    </message>
+    <!-- Pinterest -->
+    <message>
+      <source>Search Pinterest for '%1'</source>
+      <translation>Rechercher « %1 » sur Pinterest</translation>
+    </message>
+    <message>
+      <source>https://pinterest.com/search/pins/?q=%1</source>
+      <translation>https://pinterest.com/search/pins/?q=%1</translation>
+    </message>
+    <message>
+      <source>https://pinterest.com</source>
+      <translation>https://pinterest.com</translation>
+    </message>
+    <message>
+      <source>Visit the Pinterest homepage</source>
+      <translation>Consulter la page d'accueil de Pinterest</translation>
+    </message>
+    <!-- Reddit -->
+    <message>
+      <source>Visit r/%1</source>
+      <translation>Consulter r/%1</translation>
+    </message>
+    <message>
+      <source>https://reddit.com/r/%1</source>
+      <translation>https://reddit.com/r/%1</translation>
+    </message>
+    <message>
+      <source>https://reddit.com</source>
+      <translation>https://reddit.com</translation>
+    </message>
+    <message>
+      <source>Visit the front page of Reddit</source>
+      <translation>Consulter la page d'accueil de Reddit</translation>
+    </message>
+    <!-- TikTok -->
+    <message>
+      <source>Visit %1's TikTok profile</source>
+      <translation>Consulter le profil TikTok de « %1 »</translation>
+    </message>
+    <message>
+      <source>https://tiktok.com/@%1</source>
+      <translation>https://tiktok.com/@%1</translation>
+    </message>
+    <message>
+      <source>https://tiktok.com</source>
+      <translation>https://tiktok.com</translation>
+    </message>
+    <message>
+      <source>Visit your TikTok FYP</source>
+      <translation>Consulter ta page Pour toi sur TikTok</translation>
+    </message>
+    <!-- Twitch -->
+    <message>
+      <source>Visit %1's Twitch channel</source>
+      <translation>Consulter la chaîne Twitch de « %1 »</translation>
+    </message>
+    <message>
+      <source>https://twitch.tv/%1</source>
+      <translation>https://twitch.tv/%1</translation>
+    </message>
+    <message>
+      <source>https://twitch.tv</source>
+      <translation>https://twitch.tv</translation>
+    </message>
+    <message>
+      <source>Visit the Twitch homepage</source>
+      <translation>Consulter la page d'accueil de Twitch</translation>
+    </message>
+    <!-- Wikipedia -->
+    <message>
+      <source>Search Wikipedia for '%1'</source>
+      <translation>Rechercher « %1 » sur Wikipedia</translation>
+    </message>
+    <message>
+      <source>https://fr.wikipedia.org/wiki/Special:Search?search=%1</source>
+      <translation>https://fr.wikipedia.org/wiki/Special:Search?search=%1</translation>
+    </message>
+    <message>
+      <source>https://wikipedia.org</source>
+      <translation>https://wikipedia.org</translation>
+    </message>
+    <message>
+      <source>Visit the Wikipedia homepage</source>
+      <translation>Consulter la page d'accueil de Wikipedia</translation>
+    </message>
+    <!-- YouTube -->
+    <message>
+      <source>Search YouTube for '%1'</source>
+      <translation>Rechercher « %1 » sur YouTube</translation>
+    </message>
+    <message>
+      <source>https://youtube.com/results?search_query=%1</source>
+      <translation>https://youtube.com/results?search_query=%1</translation>
+    </message>
+    <message>
+      <source>https://youtube.com</source>
+      <translation>https://youtube.com</translation>
+    </message>
+    <message>
+      <source>Visit the YouTube homepage</source>
+      <translation>Consulter la page d'accueil de YouTube</translation>
+    </message>
+    <!-- X -->
+    <message>
+      <source>Visit %1's X profile</source>
+      <translation>Consulter le profil X de « %1 »</translation>
+    </message>
+    <message>
+      <source>https://x.com/%1</source>
+      <translation>https://x.com/%1</translation>
+    </message>
+    <message>
+      <source>https://x.com</source>
+      <translation>https://x.com</translation>
+    </message>
+    <message>
+      <source>Visit the X homepage</source>
+      <translation>Consulter la page d'accueil de X</translation>
+    </message>
+    <message>
+      <source>Search X for '%1'</source>
+      <translation>Rechercher « %1 » sur X</translation>
+    </message>
+    <message>
+      <source>https://x.com/search?q=%1</source>
+      <translation>https://x.com/search?q=%1</translation>
+    </message>
+  </context>
+</TS>
