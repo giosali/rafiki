020082f0b11d10a481a98803938d14ee89502152
giosali <70548443+giosali@users.noreply.github.com>
2024-12-11 20:08:42 -0600

    Remove Image enum

--- src/core/paths.cpp
+++ src/core/paths.cpp
@@ -15,55 +15,12 @@ QString Paths::GetPath(Directory d) {
     case Directory::kDataResources:
       path = "://data";
       break;
-    case Directory::kImageResources:
-      path = "://images";
-      break;
     case Directory::kUserIcons:
       path = Utilities::Combine(GetPath(Directory::kAppConfig), "icons");
       break;
   }
 
   return path;
-}
-
-QString Paths::GetPath(Image f) {
-  auto filename = QString{};
-  switch (f) {
-    case Image::kCalculator:
-      filename = "calculator.png";
-      break;
-    case Image::kFile:
-      filename = "file.svg";
-      break;
-    case Image::kFileSystemEntry:
-      filename = "filesystementry.svg";
-      break;
-    case Image::kFolder:
-      filename = "folder.svg";
-      break;
-    case Image::kQuestionMark:
-      filename = "question-mark.png";
-      break;
-    case Image::kRafiki:
-      filename = "rafiki.png";
-      break;
-    case Image::kRefresh:
-      filename = "refresh.svg";
-      break;
-    case Image::kSettings:
-      filename = "settings.svg";
-      break;
-    case Image::kTrash:
-      filename = "trash.svg";
-      break;
-    case Image::kUrl:
-      filename = "url.svg";
-      break;
-  }
-
-  return filename.isEmpty()
-           ? filename
-           : Utilities::Combine(GetPath(Directory::kImageResources), filename);
 }
 
 QString Paths::GetPath(Json f) {
--- src/core/paths.h
+++ src/core/paths.h
@@ -5,29 +5,11 @@
 
 class Paths {
  public:
-  enum class Directory {
-    kAppConfig,
-    kDataResources,
-    kImageResources,
-    kUserIcons
-  };
-  enum class Image {
-    kCalculator,
-    kFile,
-    kFileSystemEntry,
-    kFolder,
-    kQuestionMark,
-    kRafiki,
-    kRefresh,
-    kSettings,
-    kTrash,
-    kUrl
-  };
+  enum class Directory { kAppConfig, kDataResources, kUserIcons };
   enum class Json { kUserSettings, kUserWebSearches, kWebSearches };
 
   Paths() = delete;
 
   static QString GetPath(Directory d);
-  static QString GetPath(Image f);
   static QString GetPath(Json f);
 };
