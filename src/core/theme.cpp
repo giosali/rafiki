4823f278750111c38b511daf14cfc985aa5c8a85
giosali <70548443+giosali@users.noreply.github.com>
2025-01-01 21:56:18 -0600

    Add theming

---
+++ src/core/theme.cpp
@@ -0,0 +1,54 @@
+#include "theme.h"
+
+#include <QFile>
+#include <QIODeviceBase>
+#include <QJsonDocument>
+#include <QJsonObject>
+
+Theme& Theme::GetInstance() {
+  static auto instance = Theme{};
+  return instance;
+}
+
+int Theme::GetBorderRadius() const { return border_radius_; }
+
+QColor Theme::GetSelectionColor() const { return selection_color_; }
+
+QColor Theme::GetWindowBackgroundColor() const {
+  return window_background_color_;
+}
+
+QColor Theme::GetWindowForegroundColor() const {
+  return window_foreground_color_;
+}
+
+void Theme::LoadFile(const QString& filename) {
+  auto file = QFile{":/data/" + filename};
+  if (!file.open(QIODeviceBase::ReadOnly | QIODeviceBase::Text)) {
+    return;
+  }
+
+  auto document = QJsonDocument::fromJson(file.readAll());
+  if (!document.isObject()) {
+    return;
+  }
+
+  auto object = document.object();
+  if (auto key = "borderRadius"; object.contains(key)) {
+    border_radius_ = object[key].toInt();
+  }
+
+  if (auto key = "selectionColor"; object.contains(key)) {
+    selection_color_ = object[key].toString();
+  }
+
+  if (auto key = "windowBackgroundColor"; object.contains(key)) {
+    window_background_color_ = object[key].toString();
+  }
+
+  if (auto key = "windowForegroundColor"; object.contains(key)) {
+    window_foreground_color_ = object[key].toString();
+  }
+
+  emit Changed(this);
+}
---
+++ src/core/theme.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QColor>
+#include <QObject>
+#include <QString>
+
+class Theme final : public QObject {
+  Q_OBJECT
+
+ public:
+  static Theme& GetInstance();
+  int GetBorderRadius() const;
+  QColor GetSelectionColor() const;
+  QColor GetWindowBackgroundColor() const;
+  QColor GetWindowForegroundColor() const;
+  void LoadFile(const QString& filename);
+
+ signals:
+  void Changed(Theme* theme);
+
+ private:
+  Theme() = default;
+  Theme(const Theme&) = delete;
+
+  Theme operator=(const Theme&) = delete;
+
+  int border_radius_{};
+  QColor selection_color_{};
+  QColor window_background_color_{};
+  QColor window_foreground_color_{};
+};
