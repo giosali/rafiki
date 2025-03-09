ce468e5af4ffb016659c785e0ce40ffbe250fbc9
giosali <70548443+giosali@users.noreply.github.com>
2025-03-08 20:36:40 -0600

    Try launching applications with gtk-launch and then QProcess as a fallback option

--- src/core/objects/applicationobject.cpp
+++ src/core/objects/applicationobject.cpp
@@ -1,6 +1,9 @@
 #include "applicationobject.h"
 
 #include <QDesktopServices>
+#include <QFileInfo>
+#include <QProcess>
+#include <QStandardPaths>
 #include <QUrl>
 #include <cstdlib>
 #include <filesystem>
@@ -37,19 +40,56 @@ void ApplicationObject::ProcessKeyPress(const QKeyCombination& combination) {
 
       emit Hidden();
 
-      if (auto model = dynamic_cast<const ApplicationModel*>(GetModel());
-          model != nullptr) {
-        auto exec = model->GetExec().toStdString();
-
+      // `gtk-launch` is much quicker than using QProcess.
+      if (!QStandardPaths::findExecutable("gtk-launch").isEmpty()) {
         // The command in "exec" does not execute when running through VSCode.
-        // Needs to be passed by value rather than reference otherwise the exec
-        // will contain garbage characters/gibberish.
-        auto t = std::jthread{[exec] { std::system(exec.c_str()); }};
+        // The command needs to be passed by value rather than reference
+        // otherwise it will contain garbage characters/gibberish.
+        auto command = QString{"gtk-launch %1"}
+                         .arg(QFileInfo{GetDescription()}.fileName())
+                         .toStdString();
+        auto t = std::jthread{[command] { std::system(command.c_str()); }};
         t.detach();
+      } else if (auto model = dynamic_cast<const ApplicationModel*>(GetModel());
+                 model != nullptr) {
+        // Fallback to QProcess.
+        auto exec = model->GetExec();
+        auto exec_parts = exec.split(' ', Qt::SkipEmptyParts);
+        if (exec_parts.isEmpty()) {
+          qWarning() << "exec_parts is empty";
+          break;
+        }
 
-        FeatureObject::ProcessKeyPress(combination);
+        // Some applications won't launch normally when the QT_QPA_PLATFORM is
+        // set to `xcb`. This application's QT_QPA_PLATFORM value is set to
+        // `xcb` by default. Therefore, we need to look at the user's session
+        // and use that to determine whether applications should be launched
+        // using `x11` or `wayland`.
+        auto env = QProcessEnvironment::systemEnvironment();
+        auto xdg_session_type = env.value("XDG_SESSION_TYPE", "wayland");
+        auto platform = xdg_session_type == "x11" ? "x11" : "wayland";
+        env.insert("QT_QPA_PLATFORM", platform);
+
+        auto program = exec_parts.takeFirst();
+        auto process = new QProcess{};
+        process->setProgram(program);
+        process->setProcessEnvironment(env);
+        process->setArguments(exec_parts);
+
+        QObject::connect(process, &QProcess::finished, process,
+                         &QProcess::deleteLater);
+        QObject::connect(
+          process, &QProcess::errorOccurred, [=](QProcess::ProcessError error) {
+            qWarning() << "Error launching:" << program << "Error:" << error;
+            process->deleteLater();
+          });
+
+        process->startDetached();
+      } else {
+        break;
       }
 
+      FeatureObject::ProcessKeyPress(combination);
       break;
     }
     case Qt::Key_Alt:
