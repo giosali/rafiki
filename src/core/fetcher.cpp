9ced0fdf8b3c69d6f1d9a415a71b80b13b8fb158
giosali <70548443+giosali@users.noreply.github.com>
2025-03-18 13:49:41 -0500

    Improve desktop entry location retrieval handling

--- src/core/fetcher.cpp
+++ src/core/fetcher.cpp
@@ -2,42 +2,49 @@
 
 #include <QJsonArray>
 #include <QStandardPaths>
+#include <QtGlobal>
 #include <algorithm>
 #include <array>
-#include <cstdlib>
-#include <string>
 #include <system_error>
 #include <unordered_set>
 
 #include "file.h"
 #include "paths.h"
-#include "utilities.h"
 
 std::vector<std::filesystem::path> Fetcher::FetchDesktopEntryPaths() const {
   // https://wiki.archlinux.org/title/desktop_entries#Application_entry
-  // These are for applications installed system-wide.
-  auto desktop_entry_locations = std::vector<std::filesystem::path>{
-    "/usr/share/applications", "/usr/local/share/applications"};
+  // The reason why a std::unordered_set isn't used instead of a std::vector is
+  // because the order in which the locations are added matters.
+  auto desktop_entry_locations = std::vector<std::filesystem::path>{};
 
+  // From the documentation concerning the XDG Base Directory Specification:
+  // `If $XDG_DATA_DIRS is either not set or empty, a value equal to
+  // /usr/local/share/:/usr/share/ should be used.`
   // https://specifications.freedesktop.org/basedir-spec/basedir-spec-0.6.html
-  // These are typically for applications installed as Flatpaks/Snaps/etc.
-  if (auto xdg_data_dirs = std::getenv("XDG_DATA_DIRS");
-      xdg_data_dirs != nullptr) {
-    for (const auto& directory : Utilities::Split(xdg_data_dirs, ':')) {
-      auto subdirectory = std::filesystem::path{directory} / "applications";
-      if (std::find(desktop_entry_locations.begin(),
-                    desktop_entry_locations.end(),
-                    subdirectory) == desktop_entry_locations.end()) {
-        desktop_entry_locations.push_back(subdirectory);
-      }
+  //
+  // The locations containing Flatpaks/Snaps/etc should typically be found via
+  // this environment variable.
+  auto xdg_data_dirs =
+    qEnvironmentVariable("XDG_DATA_DIRS", "/usr/local/share/:/usr/share/");
+  for (const auto& directory : xdg_data_dirs.split(':')) {
+    auto subdirectory =
+      std::filesystem::path{directory.toStdString()} / "applications";
+    if (std::ranges::find(desktop_entry_locations, subdirectory) ==
+        desktop_entry_locations.end()) {
+      desktop_entry_locations.push_back(subdirectory);
     }
   }
 
-  // "~/.local/share/applications/ for user-specific applications. User entries
-  // take precedence over system entries."
-  desktop_entry_locations.push_back(
-    QStandardPaths::writableLocation(QStandardPaths::ApplicationsLocation)
-      .toStdString());
+  // `~/.local/share/applications/ for user-specific applications. User entries
+  // take precedence over system entries.`
+  if (auto local_location =
+        std::filesystem::path{
+          QStandardPaths::writableLocation(QStandardPaths::ApplicationsLocation)
+            .toStdString()};
+      std::ranges::find(desktop_entry_locations, local_location) ==
+      desktop_entry_locations.end()) {
+    desktop_entry_locations.push_back(local_location);
+  }
 
   auto desktop_entries = std::vector<std::filesystem::path>{};
   auto desktop_entry_ids = std::unordered_set<std::filesystem::path>{};
